a43929d0e73d4358cdec43d3ed2ea22bdb2a74c7
Izaro <izaro.garcia.uribe@gmail.com>
2017-11-06 20:45:14 +0100

    Ejercicio 1

---
+++ 01_perimetro_rectangulo/Perimetro de un rectangulo/src/com/izaro/Main.java
@@ -0,0 +1,28 @@
+package com.izaro;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+
+    public static int perimetroRectangulo(int base, int altura) {
+        int peri = base * altura;
+        return peri;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Introduce la base del rectángulo: ");
+        int base = Integer.parseInt(br.readLine());
+
+        System.out.println("Introduce la altura del rectángulo: ");
+        int altura = Integer.parseInt(br.readLine());
+
+        int resultado = perimetroRectangulo(base, altura);
+        System.out.println("El perimetro del rectángulo es: " + resultado);
+
+    }
+}
+
