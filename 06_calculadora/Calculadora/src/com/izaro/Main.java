435a2598b4400805631a09227152406eea36e9fa
Izaro <izaro.garcia.uribe@gmail.com>
2017-11-15 19:30:07 +0100

    Ejercicio 6

--- 06_calculadora/Calculadora/src/com/izaro/Main.java
+++ 06_calculadora/Calculadora/src/com/izaro/Main.java
@@ -9,70 +9,65 @@ import static java.lang.Math.PI;
 public class Main {
 
     public static double longitud(double radio) {
-        double resultado = 2 * radio * PI;
-        return resultado;
+        return 2 * PI * radio;
     }
 
     public static double area(double radio) {
-        double resultado = PI * Math.pow(radio, 2);
-        return resultado;
+        return PI * Math.pow(radio, 2);
     }
 
     public static double volumen(double radio) {
-        double resultado = (4 * PI * Math.pow(radio, 2) / 3);
-        return resultado;
+        return (4 * PI * Math.pow(radio, 2) / 3);
     }
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String opcion = "";
-
+        char opcion;
 
         do {
-            System.out.println("=== Calculadora ===\n" +
+            System.out.println("\n=== Calculadora ===\n" +
                     "a) Longitud de la circunferencia\n" +
                     "b) Área del círculo\n" +
                     "c) Volumen de la esfera\n" +
                     "d) Salir\n" +
                     "Opción:");
-            opcion = br.readLine();
+            opcion = br.readLine().toLowerCase().charAt(0);
+            double n = 0;
 
-            if (!opcion.equalsIgnoreCase("d")) {
-
+            if (opcion == 'a' || opcion == 'b' || opcion == 'c') {
                 System.out.println("Escribe el valor del radio para hacer el cálculo:");
-                double n = Double.parseDouble(br.readLine());
-
-                switch (opcion) {
-                    case "a":
-                    case "A":
-                        longitud(n);
-                        System.out.println("La longitud de la circunferencia es: " + longitud(n) + ".");
-                        break;
-
-                    case "b":
-                    case "B":
-                        area(n);
-                        System.out.println("El área del círculo es: " + area(n) + ".");
-                        break;
-
-                    case "c":
-                    case "C":
-                        volumen(n);
-                        System.out.println("El volumen de la esfera es: " + volumen(n) + ".");
-                        break;
-
-                    default:
-                        System.out.println("Escoge una opción correcta, por favor.");
-                        break;
-                }
+                n = Double.parseDouble(br.readLine());
             }
 
-        }
-        while (!opcion.equalsIgnoreCase("d"));
 
-        if (opcion.equalsIgnoreCase("d")) {
+            switch (opcion) {
+                case 'a':
+                    System.out.format("La longitud de la circunferencia es: %.3f.", longitud(n));
+                    break;
+
+                case 'b':
+                    System.out.format("El área del círculo es: %.3f.", area(n));
+                    break;
+
+                case 'c':
+                    System.out.format("El volumen de la esfera es: %.3f.", volumen(n));
+                    break;
+
+                case 'd':
+                    break;
+
+                default:
+                    System.out.println("Escoge una opción correcta, por favor.");
+                    break;
+            }
+
+
+        }
+        while (opcion != 'd');
+
+        if (opcion == 'd') {
             System.out.println("Has salido de la calculadora.");
         }
 
